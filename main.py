2f98bcd787b866c7935dff7164c54e6941aa9943
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-03 10:44:37 -0700

    initialize game window with fixed size and black background

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ PLAYER_SIZE = 30
 LASER_WIDTH, LASER_HEIGHT = 20, 40
 LASER_SPEED = 5
 LASER_INTERVAL = 1000  # New laser every 1000ms
+
+class LaserDodger(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Laser Dodger")
+        self.setFixedSize(WIDTH, HEIGHT)
+        self.setStyleSheet("background-color: black;")

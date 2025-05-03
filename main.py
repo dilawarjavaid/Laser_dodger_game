261357775ab1846c70b12ccd9a8cfa7db805c093
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-03 10:41:48 -0700

    Initiating game constants

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import random
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import Qt, QTimer, QRect
+
+# Game Constants
+WIDTH, HEIGHT = 400, 500
+PLAYER_SIZE = 30
+LASER_WIDTH, LASER_HEIGHT = 20, 40
+LASER_SPEED = 5
+LASER_INTERVAL = 1000  # New laser every 1000ms
